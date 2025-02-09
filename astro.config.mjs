7188952c3c1399224d2f20ecec60bd89a0bbe687
SkiuteDomantas <domantas.skiute@student.howest.be>
2025-02-09 11:57:10 +0100

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/dopaminas/',
-    trailingSlash: 'always',
-});
+    base: '/dopaminas/'});
 
